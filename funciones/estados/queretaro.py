cec8077187845a174488ff80e00b28ed9420f946
GUERRERO MARTINEZ JOSE EDUARDO <eduardo.guerrero@ine.mx>
2021-05-13 11:44:30 -0500

    antes de pasar a Raiz OK

--- funciones/estados/queretaro.py
+++ funciones/estados/queretaro.py
@@ -97,7 +97,7 @@ def elMayor(partidos):
 #**********************************
 def excel01():
     sumaColExcel01=df.groupby(by=['estados']).sum().groupby(level=[0]).cumsum().loc[estado]
-    totalDefExc = math.floor(sumaColExcel01['UNIDOS_CONTIGO']) + math.floor(sumaColExcel01['PT']) + math.floor(sumaColExcel01['VERDE']) + math.floor(sumaColExcel01['MOVIMIENTO_CIUDADANO']) + math.floor(sumaColExcel01['MORENA']) +  math.floor(sumaColExcel01['COHERENTE']) + math.floor(sumaColExcel01['NUEVA_ALIANZA']) + math.floor(sumaColExcel01['PES']) + math.floor(sumaColExcel01['RSP']) + math.floor(sumaColExcel01['FUERZA_POR_MEXICO']) +  math.floor(sumaColExcel01['RAMON_PARRA']) + math.floor(sumaColExcel01['MORENA_PT'])  + math.floor(sumaColExcel01['CANDIDATOS_NO_REGISTRADOS']) + math.floor(sumaColExcel01['VOTOS_NULOS'])
+    totalDefExc = math.floor(sumaColExcel01['PAN']) + math.floor(sumaColExcel01['PRI']) + math.floor(sumaColExcel01['PRD']) + math.floor(sumaColExcel01['MOVIMIENTO_CIUDADANO']) + math.floor(sumaColExcel01['VERDE']) + math.floor(sumaColExcel01['MORENA']) +  math.floor(sumaColExcel01['PT']) + math.floor(sumaColExcel01['QUERETARO_INDEPENDIENTE']) + math.floor(sumaColExcel01['PES']) + math.floor(sumaColExcel01['RSP']) + math.floor(sumaColExcel01['FUERZA_POR_MEXICO']) +  math.floor(sumaColExcel01['PAN_QUERETARO_INDEPENDIENTE']) + math.floor(sumaColExcel01['CANDIDATOS_NO_REGISTRADOS']) + math.floor(sumaColExcel01['VOTOS_NULOS'])
     datos=(
     ('PAN', math.floor(sumaColExcel01['PAN']),numerosLetras.numero_a_letras(math.floor(sumaColExcel01['PAN']))),
     ('PRI', math.floor(sumaColExcel01['PRI']),numerosLetras.numero_a_letras(math.floor(sumaColExcel01['PRI']))),
@@ -133,19 +133,19 @@ def excel01():
     return datos
 #**********************************
 def excel02():        
-    totalDefExc = math.floor(sumaCol['UNIDOS_CONTIGO']) + math.floor(sumaCol['PT']) + math.floor(sumaCol['VERDE']) + math.floor(sumaCol['MOVIMIENTO_CIUDADANO']) + math.floor(sumaCol['MORENA']) +  math.floor(sumaCol['COHERENTE']) + math.floor(sumaCol['NUEVA_ALIANZA']) + math.floor(sumaCol['PES']) + math.floor(sumaCol['RSP']) + math.floor(sumaCol['FUERZA_POR_MEXICO']) +  math.floor(sumaCol['RAMON_PARRA']) + math.floor(sumaCol['CANDIDATOS_NO_REGISTRADOS']) + math.floor(sumaCol['VOTOS_NULOS'])
+    totalDefExc = math.floor(sumaCol['PAN']) + math.floor(sumaCol['PRI']) + math.floor(sumaCol['PRD']) + math.floor(sumaCol['MOVIMIENTO_CIUDADANO']) + math.floor(sumaCol['VERDE']) + math.floor(sumaCol['MORENA']) +  math.floor(sumaCol['PT']) + math.floor(sumaCol['QUERETARO_INDEPENDIENTE']) + math.floor(sumaCol['PES']) + math.floor(sumaCol['RSP']) + math.floor(sumaCol['FUERZA_POR_MEXICO']) + math.floor(sumaCol['CANDIDATOS_NO_REGISTRADOS']) + math.floor(sumaCol['VOTOS_NULOS'])
     datos=(
-    ('UNIDOS_CONTIGO', math.floor(sumaCol['UNIDOS_CONTIGO']),numerosLetras.numero_a_letras(math.floor(sumaCol['UNIDOS_CONTIGO']))),
-    ('PT', math.floor(sumaCol['PT']),numerosLetras.numero_a_letras(math.floor(sumaCol['PT']))),
-    ('VERDE', math.floor(sumaCol['VERDE']),numerosLetras.numero_a_letras(math.floor(sumaCol['VERDE']))),
+    ('PAN', math.floor(sumaCol['PAN']),numerosLetras.numero_a_letras(math.floor(sumaCol['PAN']))),
+    ('PRI', math.floor(sumaCol['PRI']),numerosLetras.numero_a_letras(math.floor(sumaCol['PRI']))),
+    ('PRD', math.floor(sumaCol['PRD']),numerosLetras.numero_a_letras(math.floor(sumaCol['PRD']))),
     ('MOVIMIENTO CIUDADANO', math.floor(sumaCol['MOVIMIENTO_CIUDADANO']),numerosLetras.numero_a_letras(math.floor(sumaCol['MOVIMIENTO_CIUDADANO']))),
+    ('VERDE', math.floor(sumaCol['VERDE']),numerosLetras.numero_a_letras(math.floor(sumaCol['VERDE']))),
     ('MORENA', math.floor(sumaCol['MORENA']),numerosLetras.numero_a_letras(math.floor(sumaCol['MORENA']))),
-    ('COHERENTE', math.floor(sumaCol['COHERENTE']),numerosLetras.numero_a_letras(math.floor(sumaCol['COHERENTE']))),
-    ('NUEVA_ALIANZA', math.floor(sumaCol['NUEVA_ALIANZA']),numerosLetras.numero_a_letras(math.floor(sumaCol['NUEVA_ALIANZA']))),
+    ('PT', math.floor(sumaCol['PT']),numerosLetras.numero_a_letras(math.floor(sumaCol['PT']))),
+    ('QUERETARO_INDEPENDIENTE', math.floor(sumaCol['QUERETARO_INDEPENDIENTE']),numerosLetras.numero_a_letras(math.floor(sumaCol['QUERETARO_INDEPENDIENTE']))),
     ('PES', math.floor(sumaCol['PES']),numerosLetras.numero_a_letras(math.floor(sumaCol['PES']))),
     ('RSP', math.floor(sumaCol['RSP']),numerosLetras.numero_a_letras(math.floor(sumaCol['RSP']))),
     ('FUERZA POR MEXICO', math.floor(sumaCol['FUERZA_POR_MEXICO']),numerosLetras.numero_a_letras(math.floor(sumaCol['FUERZA_POR_MEXICO']))),
-    ('RAMON_PARRA', math.floor(sumaCol['RAMON_PARRA']),numerosLetras.numero_a_letras(math.floor(sumaCol['RAMON_PARRA']))),
     ('CANDIDATOS_NO_REGISTRADOS', math.floor(sumaCol['CANDIDATOS_NO_REGISTRADOS']),numerosLetras.numero_a_letras(math.floor(sumaCol['CANDIDATOS_NO_REGISTRADOS']))),
     ('VOTOS_NULOS', math.floor(sumaCol['VOTOS_NULOS']),numerosLetras.numero_a_letras(math.floor(sumaCol['VOTOS_NULOS']))),
     ('TOTAL', totalDefExc, numerosLetras.numero_a_letras(totalDefExc))
@@ -168,23 +168,26 @@ def excel03():
     sumaColExcel01Excel01=df.groupby(by=['estados']).sum().groupby(level=[0]).cumsum().loc[estado]    
     datos=[
     (
-        'UNIDOS_CONTIGO', math.floor(sumaColExcel01Excel01['UNIDOS_CONTIGO']),
-        numerosLetras.numero_a_letras(math.floor(sumaColExcel01Excel01['UNIDOS_CONTIGO']))
+        'PAN_QUERETARO_INDEPENDIENTE', math.floor(sumaColExcel01Excel01['PAN_QUERETARO_INDEPENDIENTE'] + sumaColExcel01Excel01['QUERETARO_INDEPENDIENTE'] + sumaColExcel01Excel01['PAN']),
+        numerosLetras.numero_a_letras(math.floor(sumaColExcel01Excel01['PAN_QUERETARO_INDEPENDIENTE'] + sumaColExcel01Excel01['QUERETARO_INDEPENDIENTE'] + sumaColExcel01Excel01['PAN']))
     ),
     (
-        'MORENA_PT', math.floor(sumaColExcel01Excel01['MORENA_PT'] + sumaColExcel01Excel01['PT'] + sumaColExcel01Excel01['MORENA']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['MORENA_PT'] + sumaColExcel01Excel01['PT'] + sumaColExcel01Excel01['MORENA'])
+        'PRI', math.floor(sumaColExcel01Excel01['PRI']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['PRI'])
     ),
     (
-        'VERDE', math.floor(sumaColExcel01Excel01['VERDE']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['VERDE'])
+        'PRD', math.floor(sumaColExcel01Excel01['PRD']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['PRD'])
     ),
     (
         'MOVIMIENTO CIUDADANO', math.floor(sumaColExcel01Excel01['MOVIMIENTO_CIUDADANO']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['MOVIMIENTO_CIUDADANO'])
     ),
     (
-        'COHERENTE', math.floor(sumaColExcel01Excel01['COHERENTE']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['COHERENTE'])
+        'VERDE', math.floor(sumaColExcel01Excel01['VERDE']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['VERDE'])
     ),
     (
-        'NUEVA_ALIANZA', math.floor(sumaColExcel01Excel01['NUEVA_ALIANZA']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['NUEVA_ALIANZA'])
+        'MORENA', math.floor(sumaColExcel01Excel01['MORENA']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['MORENA'])
+    ),
+    (
+        'PT', math.floor(sumaColExcel01Excel01['PT']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['PT'])
     ),
     (
         'PES', math.floor(sumaColExcel01Excel01['PES']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['PES'])
@@ -194,9 +197,6 @@ def excel03():
     ),
     (
         'FUERZA POR MEXICO', math.floor(sumaColExcel01Excel01['FUERZA_POR_MEXICO']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['FUERZA_POR_MEXICO'])
-    ),
-    (
-        'RAMON_PARRA', math.floor(sumaColExcel01Excel01['RAMON_PARRA']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['RAMON_PARRA'])
     ),
     (
         'CANDIDATOS_NO_REGISTRADOS', math.floor(sumaColExcel01Excel01['CANDIDATOS_NO_REGISTRADOS']), numerosLetras.numero_a_letras(sumaColExcel01Excel01['CANDIDATOS_NO_REGISTRADOS'])
@@ -293,94 +293,93 @@ tb3=excel03()
 packet = io.BytesIO()
 # create a new PDF with Reportlab
 can = canvas.Canvas(packet, pagesize=letter)
-can.drawString(133, 680, "Baja California Sur")
-can.drawString(100, 665, str(hora))
-can.drawString(253, 665, str(dia))
-can.drawString(100, 650, "CENTRO DE ESCRUTINIO Y CÓMPUTO")
-can.drawString(170, 405, tb1[0][2]) 
-can.drawString(350, 405, str(tb1[0][1])) 
-can.drawString(170, 380, tb1[1][2]) 
-can.drawString(350, 380, str(tb1[1][1])) 
-can.drawString(170, 355, tb1[2][2]) 
-can.drawString(350, 355, str(tb1[2][1])) 
-can.drawString(170, 330, tb1[3][2]) 
-can.drawString(350, 330, str(tb1[3][1])) 
-can.drawString(170, 305, tb1[4][2]) 
-can.drawString(350, 305, str(tb1[4][1])) 
-can.drawString(170, 280, tb1[5][2]) 
-can.drawString(350, 280, str(tb1[5][1])) 
-can.drawString(170, 255, tb1[6][2]) 
-can.drawString(350, 255, str(tb1[6][1]))
-can.drawString(170, 230, tb1[7][2]) 
-can.drawString(350, 230, str(tb1[7][1]))  
-can.drawString(170, 205, tb1[8][2]) 
-can.drawString(350, 205, str(tb1[8][1]))  
-can.drawString(170, 180, tb1[9][2]) 
-can.drawString(350, 180, str(tb1[9][1]))  
-can.drawString(170, 155, tb1[10][2]) 
-can.drawString(350, 155, str(tb1[10][1]))  
-can.drawString(170, 130, tb1[11][2]) 
-can.drawString(350, 130, str(tb1[11][1]))  
-can.drawString(170, 105, tb1[12][2]) 
-can.drawString(350, 105, str(tb1[12][1]))  
-can.drawString(170, 80, tb1[13][2]) 
-can.drawString(350, 80, str(tb1[13][1]))  
-can.drawString(170, 55, tb1[14][2]) 
-can.drawString(350, 55, str(tb1[14][1]))  
+can.drawString(70, 515, str(hora))
+can.drawString(200, 515, str(dia))
+can.drawString(50, 507, "CENTRO DE ESCRUTINIO Y CÓMPUTO")
+can.drawString(150, 365, tb1[0][2]) 
+can.drawString(280, 365, str(tb1[0][1])) 
+can.drawString(150, 340, tb1[1][2]) 
+can.drawString(280, 340, str(tb1[1][1])) 
+can.drawString(150, 315, tb1[2][2]) 
+can.drawString(280, 315, str(tb1[2][1])) 
+can.drawString(150, 292, tb1[3][2]) 
+can.drawString(280, 292, str(tb1[3][1])) 
+can.drawString(150, 271, tb1[4][2]) 
+can.drawString(280, 271, str(tb1[4][1])) 
+can.drawString(150, 251, tb1[5][2]) 
+can.drawString(280, 251, str(tb1[5][1])) 
+can.drawString(150, 229, tb1[6][2]) 
+can.drawString(280, 229, str(tb1[6][1]))
+can.drawString(150, 205, tb1[7][2]) 
+can.drawString(280, 205, str(tb1[7][1]))  
+can.drawString(150, 181, tb1[8][2]) 
+can.drawString(280, 181, str(tb1[8][1]))  
+can.drawString(150, 161, tb1[9][2]) 
+can.drawString(280, 161, str(tb1[9][1]))  
+can.drawString(150, 136, tb1[10][2]) 
+can.drawString(280, 136, str(tb1[10][1]))  
+can.drawString(150, 115, tb1[11][2]) 
+can.drawString(280, 115, str(tb1[11][1]))  
+can.drawString(150, 91, tb1[12][2]) 
+can.drawString(280, 91, str(tb1[12][1]))  
+can.drawString(150, 69, tb1[13][2]) 
+can.drawString(280, 69, str(tb1[13][1]))  
+can.drawString(150, 47, tb1[14][2]) 
+can.drawString(280, 47, str(tb1[14][1]))  
 #tb2  
-can.drawString(590, 659, tb2[0][2]) 
-can.drawString(760, 659, str(tb2[0][1]))  
-can.drawString(590, 635, tb2[1][2]) 
-can.drawString(760, 635, str(tb2[1][1]))  
-can.drawString(590, 611, tb2[2][2]) 
-can.drawString(760, 611, str(tb2[2][1]))  
-can.drawString(590, 587, tb2[3][2]) 
-can.drawString(760, 587, str(tb2[3][1]))  
-can.drawString(590, 566, tb2[4][2]) 
-can.drawString(760, 566, str(tb2[4][1]))  
-can.drawString(590, 542, tb2[5][2]) 
-can.drawString(760, 542, str(tb2[5][1]))  
-can.drawString(590, 518, tb2[6][2]) 
-can.drawString(760, 518, str(tb2[6][1]))  
-can.drawString(590, 497, tb2[7][2]) 
-can.drawString(760, 497, str(tb2[7][1]))  
-can.drawString(590, 474, tb2[8][2]) 
-can.drawString(760, 474, str(tb2[8][1]))  
-can.drawString(590, 451, tb2[9][2]) 
-can.drawString(760, 451, str(tb2[9][1]))  
-can.drawString(590, 428, tb2[10][2]) 
-can.drawString(760, 428, str(tb2[10][1]))  
-can.drawString(590, 405, tb2[11][2]) 
-can.drawString(760, 405, str(tb2[11][1]))  
-can.drawString(590, 382, tb2[12][2]) 
-can.drawString(760, 382, str(tb2[12][1])) 
-can.drawString(590, 359, tb2[13][2]) 
-can.drawString(760, 359, str(tb2[13][1])) 
+can.drawString(460, 517, tb2[0][2]) 
+can.drawString(600, 517, str(tb2[0][1]))  
+can.drawString(460, 497, tb2[1][2]) 
+can.drawString(600, 497, str(tb2[1][1]))  
+can.drawString(460, 477, tb2[2][2]) 
+can.drawString(600, 477, str(tb2[2][1]))  
+can.drawString(460, 457, tb2[3][2]) 
+can.drawString(600, 457, str(tb2[3][1]))  
+can.drawString(460, 439, tb2[4][2]) 
+can.drawString(600, 439, str(tb2[4][1]))  
+can.drawString(460, 422, tb2[5][2]) 
+can.drawString(600, 422, str(tb2[5][1]))  
+can.drawString(460, 405, tb2[6][2]) 
+can.drawString(600, 405, str(tb2[6][1]))  
+can.drawString(460, 387, tb2[7][2]) 
+can.drawString(600, 387, str(tb2[7][1]))  
+can.drawString(460, 370, tb2[8][2]) 
+can.drawString(600, 370, str(tb2[8][1]))  
+can.drawString(460, 352, tb2[9][2]) 
+can.drawString(600, 352, str(tb2[9][1]))  
+can.drawString(460, 334, tb2[10][2]) 
+can.drawString(600, 334, str(tb2[10][1]))  
+can.drawString(460, 317, tb2[11][2]) 
+can.drawString(600, 317, str(tb2[11][1]))  
+can.drawString(460, 298, tb2[12][2]) 
+can.drawString(600, 298, str(tb2[12][1])) 
+can.drawString(460, 279, tb2[13][2]) 
+can.drawString(600, 279, str(tb2[13][1])) 
 #Tb3
-can.drawString(590, 284, tb3[0][2]) 
-can.drawString(760, 284, str(tb3[0][1]))  
-can.drawString(590, 261, tb3[1][2]) 
-can.drawString(760, 261, str(tb3[1][1]))  
-can.drawString(590, 240, tb3[2][2]) 
-can.drawString(760, 240, str(tb3[2][1]))  
-can.drawString(590, 220, tb3[3][2]) 
-can.drawString(760, 220, str(tb3[3][1]))  
-can.drawString(590, 200, tb3[4][2]) 
-can.drawString(760, 200, str(tb3[4][1]))  
-can.drawString(590, 180, tb3[5][2]) 
-can.drawString(760, 180, str(tb3[5][1]))  
-can.drawString(590, 158, tb3[6][2]) 
-can.drawString(760, 158, str(tb3[6][1]))  
-can.drawString(590, 138, tb3[7][2]) 
-can.drawString(760, 138, str(tb3[7][1]))  
-can.drawString(590, 115, tb3[8][2]) 
-can.drawString(760, 115, str(tb3[8][1]))  
-can.drawString(590, 93, tb3[9][2]) 
-can.drawString(760, 93, str(tb3[9][1]))  
-can.drawString(590, 73, tb3[10][2]) 
-can.drawString(760, 73, str(tb3[10][1]))  
-can.drawString(590, 53, tb3[11][2]) 
-can.drawString(760, 53, str(tb3[11][1]))  
+can.drawString(460, 235, tb3[0][2]) 
+can.drawString(600, 235, str(tb3[0][1]))  
+can.drawString(460, 219, tb3[1][2]) 
+can.drawString(600, 219, str(tb3[1][1]))  
+can.drawString(460, 200, tb3[2][2]) 
+can.drawString(600, 200, str(tb3[2][1]))  
+can.drawString(460, 183, tb3[3][2]) 
+can.drawString(600, 183, str(tb3[3][1]))  
+can.drawString(460, 166, tb3[4][2]) 
+can.drawString(600, 166, str(tb3[4][1]))  
+can.drawString(460, 149, tb3[5][2]) 
+can.drawString(600, 149, str(tb3[5][1]))  
+can.drawString(460, 132, tb3[6][2]) 
+can.drawString(600, 132, str(tb3[6][1]))  
+can.drawString(460, 115, tb3[7][2]) 
+can.drawString(600, 115, str(tb3[7][1]))  
+can.drawString(460, 98, tb3[8][2]) 
+can.drawString(600, 98, str(tb3[8][1]))  
+can.drawString(460, 79, tb3[9][2]) 
+can.drawString(600, 79, str(tb3[9][1]))  
+can.drawString(460, 61, tb3[10][2]) 
+can.drawString(600, 61, str(tb3[10][1]))  
+can.drawString(460, 42, tb3[11][2]) 
+can.drawString(600, 42, str(tb3[11][1]))  
 can.save()
 
 #move to the beginning of the StringIO buffer
